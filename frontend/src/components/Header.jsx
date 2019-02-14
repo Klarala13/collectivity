39b59b8d44696e083d5b428e28ad08616e1aaba5
Klara <sanchez.clara13@gmail.com>
2019-02-14 18:24:58 +0100

    npm install and bootstrap

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return <h1>Hey There! Welcome to Collectivity</h1>;
+  }
+};
+
+export default Header;
---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import Header from "./components/Header";
+
+render(<Header />, document.querySelector("#main"));
