ddfc392ac9e6ca557461a4065f2be76621411254
Klara <sanchez.clara13@gmail.com>
2019-02-14 18:26:06 +0100

    imported bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import Header from "./components/Header";
 
+import "./sass/style.scss";
+
 render(<Header />, document.querySelector("#main"));
