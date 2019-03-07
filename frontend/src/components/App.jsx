fe2339db056307e6ebd621b27998b181b568a102
Klara <sanchez.clara13@gmail.com>
2019-03-07 10:30:54 +0100

    created Nav component added it to App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Nav from "./Nav";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
+        <Nav />
         <Header />
       </div>
     );
