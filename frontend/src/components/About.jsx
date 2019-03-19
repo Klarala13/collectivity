51acf8d49a3f99f19449ba5300945042e79674d0
Klara <sanchez.clara13@gmail.com>
2019-03-19 19:44:21 +0100

    creaded aboutUs content missing

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,7 +4,7 @@ import NavBar from "./NavBar";
 
 class About extends React.Component {
   componentDidMount() {
-    document.title = "Abput Us";
+    document.title = "About Us";
   }
 
   render() {
