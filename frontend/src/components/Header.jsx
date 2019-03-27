406c15b8b957d5b8dd11ffc58524db93727cf6e1
Klara <sanchez.clara13@gmail.com>
2019-03-27 15:00:15 +0100

    formated header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,10 +6,9 @@ class Header extends React.Component {
       <div>
     <h1>Hey There! Welcome to Collectivity</h1>
     <p>Collectivity is a platform that tries to promote a collective lifestyle that alienates us from capitalism.
-    You can become a member and actively share and participate, or you can donate or request something as a one time thing. 
+    You can become a member and actively share and participate, or <b>you can donate or request something as a one time thing</b>. 
     We encourage you to try it out and find the joy in sharing.</p>
     </div>
     )}
 };
-
 export default Header;
