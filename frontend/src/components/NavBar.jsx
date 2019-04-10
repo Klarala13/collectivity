a518280d2d6b16e61ee2dda2dbf40b2fd332a24e
Elise Maschke <elise.maschke@outlook.de>
2019-04-10 16:10:12 +0200

    started navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,23 +3,52 @@ import React from "react";
 // import TimeBank from "./TimeBank";
 
 class NavBar extends React.Component {
-
   render() {
     return (
-      <div className="container">
-        <div 
-          role="navigation" 
-          className="col-lg-12 navbar-expand{-sm|-md|-lg|-xl}"
-        >
-            <a className="nav navbar-expand{-sm|-md|-lg|-xl}" >Hey!</a>
-            <a className="nav navbar-expand{-sm|-md|-lg|-xl}" >TimeBank </a>
-            {/* <TimeBank /> */}
-            <a className="nav navbar-expand{-sm|-md|-lg|-xl}" >Freebies</a>
-            <a className="nav navbar-expand{-sm|-md|-lg|-xl}" >Login</a>
-            <a className="nav navbar-expand{-sm|-md|-lg|-xl}" >About</a>
-        </div>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mr-auto nav mr-auto justify-content-center">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Hey!
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  TimeBank
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Freebies
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
         {/* <Header /> */}
-
       </div>
     );
   }
