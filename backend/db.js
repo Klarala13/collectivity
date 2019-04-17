499a3fab18cd2809808bc01ceb0b584e374a16c1
Klara <sanchez.clara13@gmail.com>
2019-04-17 14:31:08 +0200

    not using db.js, commented it out

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,13 @@
-const path = require("path");
+// const path = require("path");
 
-const lowdb = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
+// // const lowdb = require("lowdb");
+// // const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync(
-  path.join(__dirname, "data", "users.json")
-);
+// // const adapter = new FileSync(
+// //   path.join(__dirname, "data", "users.json")
+// // );
 
-const db = lowdb(adapter);
-db.defaults({ students: [] }).write();
+// const db = lowdb(adapter);
+// db.defaults({ students: [] }).write();
 
-module.exports = db;
+// module.exports = db;
