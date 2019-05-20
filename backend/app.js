49db11b0db52055a2831a055c83a88f74e1a8c2b
Elise Maschke <elise.maschke@outlook.de>
2019-05-20 12:05:47 +0200

    abstract user acttions in controller, docs

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,15 @@
-require('dotenv').config({path: __dirname + '/.env'});
+require("dotenv").config({ path: __dirname + "/.env" });
 const express = require("express");
 const logger = require("morgan");
 const createError = require("http-errors");
 
 const { setCorsHeaders } = require("./middleware/security");
 
-const indexRouter = require("./routes/index");
-const itemsRouter = require("./routes/items");
-const usersRouter = require("./routes/users");
-
 const { genericErrors } = require("./lib/controllers/messageController");
 
 const app = express();
+
+const usersRouter = require("./routes/users");
 
 const postgres = require("pg");
 const { Client } = require("pg");
@@ -29,35 +27,36 @@ client.connect();
 
 // promise
 
-async function seedAdmin () {
+async function seedAdmin() {
   return client
-      .query("select * from public.users")
-      .then(res => {
-        console.log("Hey", res.row);
-        console.log("User's name:", res.rows[0].lastName);
-        if (
-          res.rowCount >= 1 &&
-          res.rows.filter(user => user.email === "admin@dci.de").length > 0
-        ) {
-          console.log("All users:", res.rows);
-        } else {
-          client.query(
-            `INSERT INTO public.users("firstName", "lastName", "email", "password", "city", "zipCode", "registrationDate", "rating") 
+    .query("select * from public.users")
+    .then(res => {
+   
+      console.log("User's name:", res.rows[0].lastName);
+      if (
+        res.rowCount >= 1 &&
+        res.rows.filter(user => user.email === "admin@dci.de").length > 0
+      ) {
+        console.log("All users:", res.rows);
+      } else {
+        client.query(
+          `INSERT INTO public.users("firstName", "lastName", "email", "password", "city", "zipCode", "registrationDate", "rating") 
         VALUES ('The', 'Admin', 'admin@dci.de', '123456', 'Berlin', 10234, '2019-05-04', 5)`
-          );
-          console.log("Admin seeded");
-        }
-      })
-      .catch(e => console.error(e.stack));
+        );
+        console.log("Admin seeded");
+      }
+    })
+    .catch(e => console.error(e.stack));
 }
 
-client.query("SELECT to_regclass('public.users')").then( async res => {
+client.query("SELECT to_regclass('public.users')").then(async res => {
   console.log("RES", res.rows);
   if (res.rows[0].to_regclass !== null) {
     const response = await seedAdmin();
   } else {
-    client.query(
-      `CREATE TABLE public."users"
+    client
+      .query(
+        `CREATE TABLE public."users"
       (
           "firstName" character varying(30) COLLATE pg_catalog."default" NOT NULL,
           "lastName" character varying(30) COLLATE pg_catalog."default" NOT NULL,
@@ -76,8 +75,8 @@ client.query("SELECT to_regclass('public.users')").then( async res => {
       ALTER TABLE public."users"
           OWNER to postgres;
       `
-    )
-    .then(() => seedAdmin())
+      )
+      .then(() => seedAdmin());
   }
 });
 
@@ -88,9 +87,35 @@ app.use(setCorsHeaders);
 app.use(logger("dev"));
 app.use(express.json());
 
-app.use("/", indexRouter);
+app.get("/", function(req, res) {
+  console.log("Welcome to Collectivity")
+  res.json({
+    message: "Welcome to Collectivity! We're very happy to see you here"
+  });
+});
+
 app.use("/users", usersRouter);
-app.use("/items", itemsRouter);
+// app.get("/users", async function(req, res, next) {
+//   console.log("users");
+//   try {
+//     const userQuery = "select * from public.users";
+//     await client.query(userQuery).then(response => {
+//       // console.log("query", userQuery);
+//       // console.log("res", res.rows);
+
+//       const user = response.rows[0];
+//       res.send(user);
+//     });
+//   } catch (e) {
+//     console.log("ERROR", e);
+//     next(e);
+//   }});
+app.get("/items", function(req, res) {
+  res.json({ message: "Item list" });
+});
+app.get("/services", function(req, res) {
+  res.json({ message: "Service list" });
+});
 
 // Catch any route that is not recognized
 app.use((req, res, next) => {
--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { welcomeMessage } = require("../lib/controllers/messageController");
-
-router.get("/", welcomeMessage);
-console.log("Welcome to Collectivity! App running on localhost 4001");
-
-module.exports = router;
