25c3b4b89df13a8e2b6555660cf0c0b4f3922f15
leenaMKA <leena.mkarem@gmail.com>
2019-05-21 14:07:52 +0200

    redirect the profile after login

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -3,21 +3,9 @@ import React, { Component } from "react";
 class Profile extends Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Col xs={6} md={4}>
-            <Image src="holder.js/171x180" roundedCircle />
-          </Col>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Row>
-      </Container>
+      <div>
+        PROFILE
+      </div>
     );
   }
 }
