ed13fd9628ac0d5700f576d745e6130ea47ae597
leenaMKA <leena.mkarem@gmail.com>
2019-05-21 16:25:20 +0200

    Add image

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-
+import Leena from "../assets/Leena.jpg";
 class Profile extends Component {
   render() {
     return (
-      <div>
-        PROFILE
+      <div className="card-deck">
+        <div className="card">
+          <img className="rounded-circle" src={Leena} alt="Logo" />
+          <div className="card-body">
+            <div className="card-title h3">card Title</div>
+            <p className="card-text">ghhgjuiuiikijjh</p>
+            <div className="card-footer">
+              <small className="text-muted">Last updat 30 min</small>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 export default Profile;
