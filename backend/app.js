c97d63a4487333231c9cc315b61662f4be570f1e
Elise Maschke <elise.maschke@outlook.de>
2019-05-23 16:53:42 +0200

    Creates freebies table and seeded freebies

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(setCorsHeaders);
 
 const usersRouter = require("./routes/users");
-const freebiesRouter = require("./routes/freebies")
-const timebanksRouter = require("./routes/timebanks")
+const freebiesRouter = require("./routes/freebies");
+const timebanksRouter = require("./routes/timebanks");
 
 const { Client } = require("pg");
 
@@ -49,6 +49,8 @@ async function seedAdmin() {
     .catch(e => console.error(e.stack));
 }
 
+// Creation of users table
+
 client.query("SELECT to_regclass('public.users')").then(async res => {
   if (res.rows[0].to_regclass !== null) {
     const response = await seedAdmin();
@@ -73,6 +75,71 @@ client.query("SELECT to_regclass('public.users')").then(async res => {
       TABLESPACE pg_default;
 
         ALTER TABLE public.users
+            OWNER to postgres;
+      `
+      )
+      .then(() => seedAdmin());
+  }
+});
+
+async function seedFreebies() {
+  return client
+    .query("select * from public.freebies")
+    .then(res => {
+      console.log("Let's seed some freebies")
+      if (
+        res.rowCount >= 1 &&
+        res.rows.filter(freebie => freebie.item === "Ball").length > 0
+      ) {
+        console.log("All freebies:", res.rows);
+      } else {
+        client.query(
+        `INSERT INTO public.freebies("item", "description", "image", "zipCode", "location", "category", "user") 
+        VALUES ('Ball', 'My old football', 'https://images.pexels.com/photos/1342252/pexels-photo-1342252.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', '12345', 'My house', 'Sports', 1);
+        INSERT INTO public.freebies("item", "description", "image", "zipCode", "location", "category", "user") 
+        VALUES ('Cup', 'Colorful, not very used', 'https://images.pexels.com/photos/433199/pexels-photo-433199.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', '12345', 'My house', 'House&Garden', 1)`
+        );
+        console.log("Freebies seeded");
+      }
+    })
+    .catch(e => console.error(e.stack));
+}
+
+// Creation of freebies table
+
+client.query("SELECT to_regclass('public.freebies')").then(async res => {
+  console.log("RES", res.rows);
+  // check if table freebies already exists
+  if (res.rows[0].to_regclass !== null) {
+    client.query("SELECT to_regclass('public.users')").then(async resp => {
+      //check if table users exists for the foreign key 
+      console.log("RESP", resp.rows);
+      if (resp.rows[0].to_regclass !== null) {
+        const response = await seedFreebies();
+      }
+    });
+  } else {
+    client
+      .query(
+        `CREATE TABLE public.freebies
+        (   "itemId" SERIAL PRIMARY KEY,
+            "item" character varying (30) NOT NULL,
+            "description" character varying (300),
+            "image" character varying,
+            "zipCode" integer,
+            "location" character varying,
+            "category" character varying NOT NULL 
+            CHECK (category IN ('House&Garden', 'Fashion', 'Motors', 'Entertainment', 'Electronics', 'Art/Collectibles', 'Sports', 'Toys', 'Media', 'Pets', 'Others')),
+            "user" integer NOT NULL,
+            CONSTRAINT "User" FOREIGN KEY ("user")
+            REFERENCES public.users ("userId") MATCH SIMPLE
+        )
+        WITH (
+          OIDS = FALSE
+      )
+      TABLESPACE pg_default;
+
+        ALTER TABLE public.freebies
             OWNER to postgres;
       `
       )
