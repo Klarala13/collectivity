8de128147c11a14e79483986c8a287827c26cc1b
Klara <sanchez.clara13@gmail.com>
2019-05-23 22:40:53 +0200

    installed font awesome

--- frontend/src/components/imageUpload/Spinner.jsx
+++ frontend/src/components/imageUpload/Spinner.jsx
@@ -1,8 +1,9 @@
-import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBowlingBall } from "@fortawesome/free-solid-svg-icons"
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-export default () => 
+export default () => (
   <div className="spinner fadein">
-    <FontAwesomeIcon icon={faBowlingBall} size="5x" color="3B5998" />
+    <FontAwesomeIcon icon={faSpinner} size="5x" color="3B5998" />
   </div>
+);
