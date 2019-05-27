decb1b140d782d0745874ad8ade551dfc0b10539
leenaMKA <leena.mkarem@gmail.com>
2019-05-27 12:09:24 +0200

    fix: remove unused, maybe copied folder @elise

--- src/components/imageUpload/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBowlingBall } from "@fortawesome/free-solid-svg-icons"
-
-export default () => 
-  <div className="spinner fadein">
-    <FontAwesomeIcon icon={faBowlingBall} size="5x" color="3B5998" />
-  </div>
