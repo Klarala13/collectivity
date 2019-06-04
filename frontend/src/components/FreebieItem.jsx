a543bf4ddc0573d140cc996f61d6b36fda249b09
Maximilian <maximilian.meeks@gmail.com>
2019-06-04 15:45:58 +0200

    fetching freebies from database

--- frontend/src/components/FreebieItem.jsx
+++ frontend/src/components/FreebieItem.jsx
@@ -3,18 +3,17 @@ import React, { Component } from "react";
 class Item extends Component {
   render() {
     return (
-      <tr>
-        <th scope="row">{this.props.data.id}</th>
-        <td>
-          <img
-            src={this.props.data.imageUrl}
-            style={{ width: "100px" }}
-            alt={this.props.data.name}
-          />
-        </td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.header}</td>
-      </tr>
+        <div className="card mx-3" style={{ width: "15rem" }}>
+          <img className="card-img-top" src={this.props.data.image} alt={this.props.data.item}/>
+          <div className="card-body">
+            <h5 className="card-title">{this.props.data.item}</h5>
+            <p className="card-text ellipsis">{this.props.data.description}</p>
+            <a href="#" className="btn btn-freebie">
+              Request Freebie
+              <i className="mx-2 fas fa-angle-double-right icon-align-middle"></i>
+            </a>
+          </div>
+        </div>
     );
   }
 }
