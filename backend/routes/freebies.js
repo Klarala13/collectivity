75f414ac7c9ba5164553b101c5b38dea6292c2b2
Elise Maschke <elise.maschke@outlook.de>
2019-06-06 15:50:52 +0200

    app.js for cors?

--- backend/routes/freebies.js
+++ backend/routes/freebies.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const app = require("../app");
 
 // Connection to postgreSQL
 
--- backend/routes/skills.js
+++ backend/routes/skills.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const app = require("../app");
 
 // Connection to postgreSQL
 
