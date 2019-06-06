2bbc9f6294b2392b9cb98a364b06b2278fb633dd
Elise Maschke <elise.maschke@outlook.de>
2019-06-06 14:02:05 +0200

    first commit

--- backend/routes/skills.js
+++ backend/routes/skills.js
@@ -30,44 +30,44 @@ const listSkills = (req, res, next) => {
   }
 };
 
-const addSkill = (req, res, next) => {
-  console.log("req.body", req.body);
-  try {
-    const today = new Date();
-    const date =
-      today.getFullYear() +
-      "-" +
-      (today.getMonth() + 1) +
-      "-" +
-      today.getDate();
-    const {
-      skill,
-      description,
-      location,
-      time_span,
-      category,
-      user_id
-    } = req.body;
-    // TODO connect with user_id
-    client.query(
-      `INSERT INTO public.skills("skill", "description", "location", "time_span", "category", "user_id") 
-      VALUES ('${skill}', '${description}', '${location}', '${Number(
-        time_span
-      )}', '${category}', '${Number(user_id)}')`
-    );
-    console.log("New skill seeded");
-    console.log("request", req.body);
+// const addSkill = (req, res, next) => {
+//   console.log("req.body", req.body);
+//   try {
+//     const today = new Date();
+//     const date =
+//       today.getFullYear() +
+//       "-" +
+//       (today.getMonth() + 1) +
+//       "-" +
+//       today.getDate();
+//     const {
+//       skill,
+//       description,
+//       location,
+//       time_span,
+//       category,
+//       user_id
+//     } = req.body;
+//     // TODO connect with user_id
+//     client.query(
+//       `INSERT INTO public.skills("skill", "description", "location", "time_span", "category", "user_id") 
+//       VALUES ('${skill}', '${description}', '${location}', '${Number(
+//         time_span
+//       )}', '${category}', '${Number(user_id)}')`
+//     );
+//     console.log("New skill seeded");
+//     console.log("request", req.body);
 
-    const skillsQuery = "select * from public.skills";
-    client.query(skillsQuery).then(response => {
-      const newSkill = response.rows;
-      res.send(newSkill);
-    });
-  } catch (e) {
-    console.log("ERROR", e);
-    next(e);
-  }
-};
+//     const skillsQuery = "select * from public.skills";
+//     client.query(skillsQuery).then(response => {
+//       const newSkill = response.rows;
+//       res.send(newSkill);
+//     });
+//   } catch (e) {
+//     console.log("ERROR", e);
+//     next(e);
+//   }
+// };
 
 router
   .route("/")
