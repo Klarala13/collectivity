e7db063debe5a4c0c2a8465a2c25b66af654942e
Elise Maschke <elise.maschke@outlook.de>
2019-06-11 16:12:41 +0200

    post for later

--- backend/routes/freebies.js
+++ backend/routes/freebies.js
@@ -65,47 +65,47 @@ const getFreebieUser = (req, res, next) => {
   }
 };
 
-const addFreebie = (req, res, next) => {
-  console.log("req.body", req.body);
-  try {
-    const today = new Date();
-    const date =
-      today.getFullYear() +
-      "-" +
-      (today.getMonth() + 1) +
-      "-" +
-      today.getDate();
-    const {
-      skill,
-      description,
-      location,
-      time_span,
-      category,
-      user_id
-    } = req.body;
-    // TODO connect with user_id
-    client.query(
-      `INSERT INTO public.freebies("item", "description", "image", "zip_code", "location", "category", "user_id") 
-      VALUES ('${item}', '${description}', '${image}', '${image}', '${location}', '${category}', '${Number(user_id)}')`
-    );
-    console.log("New skill seeded");
-    console.log("request", req.body);
+// const addFreebie = (req, res, next) => {
+//   console.log("req.body", req.body);
+//   try {
+//     const today = new Date();
+//     const date =
+//       today.getFullYear() +
+//       "-" +
+//       (today.getMonth() + 1) +
+//       "-" +
+//       today.getDate();
+//     const {
+//       item,
+//       description,
+//       location,
+//       zip_code,
+//       category,
+//       user_id
+//     } = req.body;
+//     // TODO connect with user_id
+//     client.query(
+//       `INSERT INTO public.freebies("item", "description", "image", "zip_code", "location", "category", "user_id") 
+//       VALUES ('${item}', '${description}', '${image}', '${Number(zip_code)}', '${location}', '${category}', '${Number(user_id)}')`
+//     );
+//     console.log("New skill seeded");
+//     console.log("request", req.body);
 
-    const freebieQuery = "select * from public.freebies";
-    client.query(freebieQuery).then(response => {
-      const newSkill = response.rows;
-      res.send(newSkill);
-    });
-  } catch (e) {
-    console.log("ERROR", e);
-    next(e);
-  }
-};
+//     const freebieQuery = "select * from public.freebies";
+//     client.query(freebieQuery).then(response => {
+//       const newSkill = response.rows;
+//       res.send(newSkill);
+//     });
+//   } catch (e) {
+//     console.log("ERROR", e);
+//     next(e);
+//   }
+// };
 
 router
   .route("/")
   .get(listFreebies)
-  .post(addFreebie);
+  // .post(addFreebie);
 router.route("/one").post(getFreebieById)
 router.route("/user").post(getFreebieUser)
 
